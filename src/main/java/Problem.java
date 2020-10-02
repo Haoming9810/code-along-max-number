f9a572515ae5fc9a7c01ce4b548aba3008da38fb
Peng Du <rick.peng.du@gmail.com>
2020-10-02 00:11:51 -0700

    add function

--- src/main/java/Problem.java
+++ src/main/java/Problem.java
@@ -1,8 +1,16 @@
-public class Problem1 {
+public class Problem {
+    public static int findMax(final int[] input) {
+        if (input == null || input.length == 0) {
+            return -1;
+        }
 
-    // Do not change signature (function name, parameters)
-    public static int binarySearch(int[] data, int target) {
-        // Add your solution
-        return -1; // Place holder
+        int maxVal = input[0], maxIdx = 0;
+        for (int i = 0; i < input.length; i++) {
+            if (maxVal < input[i]) {
+                maxVal = input[i];
+                maxIdx = i;
+            }
+        }
+        return maxIdx;
     }
 }
--- src/test/java/ProblemTest.java
+++ src/test/java/ProblemTest.java
@@ -2,32 +2,30 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class Problem1Test {
+public class ProblemTest {
 
     // Example test
     // DO NOT CHANGE
     @Test
     public void testBinarySearch() {
-        int inputs[][] = {
+        int[][] inputs = {
                 {},
                 {3},
-                {3},
-                {1,3},
-                {1,3},
-                {4,6,8},
-                {4,6,8},
-                {4,6,8},
-                {3,5,7,9,20},
-                {3,5,7,9,20},
+                {1, 1},
+                {1, 3},
+                {3, 3},
+                {1, 2, 3},
+                {3, 2, 1},
+                {3, 3, 3},
+                {20, 7, 9, -2, -3},
         };
-        int targets[] = {9, 1, 3, 1, 3, 4, 6, 8, 3, 9};
-        int answer[] = {-1, -1, 0, 0, 1, 0, 1, 2, 0, 3};
+        int[] answer = {-1, 0, 0, 1, 0, 2, 0, 0, 0};
 
-        assertEquals(inputs.length, targets.length);
         assertEquals(inputs.length, answer.length);
 
         for (int i=0; i<inputs.length; i++) {
-            int actual = Problem1.binarySearch(inputs[i], targets[i]);
+            int actual = Problem.findMax(inputs[i]);
+
             int expected = answer[i];
             assertEquals(expected, actual);
         }
