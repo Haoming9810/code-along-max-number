8623ae370e7b16763445901961aba094865a8694
Peng Du <rick.peng.du@gmail.com>
2020-10-03 15:05:53 -0700

    add tests

--- src/main/java/Problem.java
+++ src/main/java/Problem.java
@@ -1,15 +1,5 @@
 public class Problem {
     public static int findMax(final int[] input) {
-        if (input == null || input.length == 0) {
-            return -1;
-        }
-
-        int maxIdx = 0;
-        for (int i = 1; i < input.length; i++) {
-            if (input[maxIdx] < input[i]) {
-                maxIdx = i;
-            }
-        }
-        return maxIdx;
+        return -1;
     }
 }
--- src/test/java/ProblemTest.java
+++ src/test/java/ProblemTest.java
@@ -1,4 +1,3 @@
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -6,31 +5,24 @@ import static org.junit.Assert.assertEquals;
 public class ProblemTest {
     @Test
     public void testFindMaxNumber() {
-        int[][] inputs = {
-                {},
-                {3},
-                {1, 1},
-                {1, 3},
-                {3, 3},
-                {1, 2, 3},
-                {3, 2, 1},
-                {3, 3, 3},
-                {20, 7, 9, -2, -3},
-        };
-        int[] answer = {-1, 0, 0, 1, 0, 2, 0, 0, 0};
+        int[] input1 = {12, 12, 1, 7, 18, 10, 3};
+        int actual = Problem.findMax(input1);
+        assertEquals(4, actual);
 
-        assertEquals(inputs.length, answer.length);
+        int[] input2 = {-9, -2, -1, 7};
+        actual = Problem.findMax(input2);
+        assertEquals(2, actual);
 
-        for (int i = 0; i < inputs.length; i++) {
-            int actual = Problem.findMax(inputs[i]);
+        int[] input3 = {12, 12, 1, 7, 15, 15, 3};
+        actual = Problem.findMax(input3);
+        assertEquals(4, actual);
 
-            int expected = answer[i];
-            try {
-                assertEquals(expected, actual);
-            } catch (AssertionError e) {
-                System.out.println("test case " + i + " failed: " + e);
-                Assert.fail();
-            }
-        }
+        int[] input4 = {};
+        actual = Problem.findMax(input4);
+        assertEquals(-1, actual);
+
+        int[] input5 = null;
+        actual = Problem.findMax(input5);
+        assertEquals(-1, actual);
     }
 }
